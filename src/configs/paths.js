8af0b4246dddbd7892ea70513176e10eb1cbdc02
dzivenu <makafuigdzivenu@gmail.com>
2018-09-18 14:21:28 +0000

    add styles and scripts paths, clean up

--- src/configs/paths.js
+++ src/configs/paths.js
@@ -1,35 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-
-const root = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(root, relativePath);
-const resolveRuntime = relativePath => path.resolve(fs.realpathSync(process.cwd()), relativePath);
+const fs = require('fs'),
+    path = require('path'),
+    root = fs.realpathSync(process.cwd()),
+    resolveApp = relativePath => path.resolve(root, relativePath),
+    resolveRuntime = relativePath => path.resolve(fs.realpathSync(process.cwd()), relativePath);
 
 module.exports = {
 	dir: resolveApp('.'),
 	app: resolveApp('./src/app.js'),
-	ui: resolveApp('./src/client/ui.js'),
-	db_setup: resolveApp('./sql/db-manager.js'),
+	styles: [ resolveApp('./node_modules/semantic-ui-css/semantic.min.css'), resolveApp('./src/configs/global.css') ],
+	scripts: resolveApp('./src/client/ui/js.js'),
 	login: resolveApp('./src/client/pages/login.js'),
 	config: resolveApp('./configs/config.js'),
-	index_p: resolveApp('./src/client/pages/index.js'),
-	projects_p: resolveApp('./src/client/pages/projects.js'),
-	project_p: resolveApp('./src/client/pages/project.js'),
-	reports_p: resolveApp('./src/client/pages/reports.js'),
+	index: resolveApp('./src/client/pages/index.js'),
+	projects: resolveApp('./src/client/pages/projects.js'),
+	project: resolveApp('./src/client/pages/project.js'),
+	reports: resolveApp('./src/client/pages/reports.js'),
 	project_home: resolveApp('./src/client/pages/partials/project-home.js'),
 	project_reports: resolveApp('./src/client/pages/partials/project-reports.js'),
 	project_queries: resolveApp('./src/client/pages/partials/project-queries.js'),
 	project_members: resolveApp('./src/client/pages/partials/project-members.js'),
 	project_settings: resolveApp('./src/client/pages/partials/project-settings.js'),
-	peers_p: resolveApp('./src/client/pages/peers.js'),
-	peer_p: resolveApp('./src/client/pages/peer.js'),
-	steem_p: resolveApp('./src/client/pages/steem.js'),
+	peers: resolveApp('./src/client/pages/peers.js'),
+	peer: resolveApp('./src/client/pages/peer.js'),
+	steem: resolveApp('./src/client/pages/steem.js'),
 	report: resolveApp('./src/client/pages/report.js'),
 	queries: resolveApp('./src/client/pages/queries.js'),
 	create_report: resolveApp('./src/client/pages/create-report.js'),
 	create_query: resolveApp('./src/client/pages/create-query.js'),
-	create_project: resolveApp('./src/client/pages/create-project.js'),
-	cors: resolveApp('./configs/cors.js'),
+	create_project: resolveApp('./src/client/pages/create-project.js'),\
 	public: resolveApp('./public'),
 	server: resolveApp('./src/server/server.js'),
 	client: resolveApp('./src/client/index.js'),
--- src/configs/webpack-entries.js
+++ src/configs/webpack-entries.js
@@ -4,25 +4,26 @@ var paths = require('./paths.js');
 var entry = {};
 
 entry = {
-	ui: paths.ui,
+	styles: paths.styles,
+	scripts: paths.scripts,
 	login: paths.login,
-    index_p: paths.index_p,
-	projects_p: paths.projects_p,
+    index: paths.index,
+	projects: paths.projects,
 	queries: paths.queries,
-	project_page: paths.project_p,
+	project: paths.project,
 	project_home: paths.project_home,
 	project_reports: paths.project_reports,
 	project_members: paths.project_members,
 	project_settings: paths.project_settings,
-	reports_p: paths.reports_p,
-	peers_p: paths.peers_p,
-	peer_p: paths.peer_p,
-	steem_p: paths.steem_p,
+	reports: paths.reports,
+	peers: paths.peers,
+	peer: paths.peer,
+	steem: paths.steem,
 	report: paths.report,
 	create_report: paths.create_report,
 	create_query: paths.create_query,
 	create_project: paths.create_project,
-	project_queries: paths.project_queries,
+	project_queries: paths.project_queries
 }
     
 module.exports = entry;
