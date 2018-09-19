977c02edadd067a722d7bb35f5150562ed0b48c9
dzivenu <makafuigdzivenu@gmail.com>
2018-09-19 18:52:22 +0000

    -bump! add voting/unvoting, comment sorting, voter listing

--- src/client/pages/report.js
+++ src/client/pages/report.js
@@ -1,7 +1,8 @@
 
 'use strict';
 
-var dsteem = require('dsteem'),
+var sc2 = require('sc2-sdk'),
+	dsteem = require('dsteem'),
 	templator = require('../templator'),
 	config = require('../../configs/config'),
 	utils = require('../../lib/utils'),
@@ -17,26 +18,70 @@ var dsteem = require('dsteem'),
 	
 	try {
 		
-		var permlink = window.location.pathname.split('/')[2];
-		var author = permlink.split('-')[0];
+        
+        
+        var post_category = "";
+        var parent_author = "";
+        var parent_permlink = "";
+        
+        const access_token = await Promise.resolve(sessionStorage.access_token);
 		
-		var type = "writes in";
+		if(!active_user || active_user === '') { alert('Sorry not logged in. Please login and try again.'); window.location.href = '/login' };
+			
+		let steem_api = sc2.Initialize({
+			app: config.sc2_app_name,
+			callbackURL: window.location.href,
+			accessToken: access_token,
+			scope: config.sc2_scope_array
+		});
 		
-		/*
-		if (category == "proposal") type = "proposes in";
-		if (category == "question") type = "questions in";
-		if (category == "contest") type = "contests in";
-		if (category == "quiz") type = "quizzes in";
-		if (category == "gig") type = "outsources in";
-		*/
+    
+		const permlink = window.location.pathname.split('/')[2];
+		const author = permlink.split('-')[0];
 		
 		const report = await client.database.call('get_content', [author, permlink]);
-		
-		
+		const author_earnings = Math.max(Number(report.pending_payout_value.split(' ')[0]), Number(report.total_payout_value.split(' ')[0]) + Number(report.curator_payout_value.split(' ')[0])).toFixed(2);
+        
+        //do some nasty DOM manipulations
+        document.title = report.title + " - Peer Query";    //set title again, for the sake of posts not created on pq
+		document.getElementById('last_active').title = new Date(report.active).toDateString();
+		document.getElementById('last_active').innerText = timeago.format(report.active);
+		document.getElementById('author_earnings').innerText = "$" + author_earnings + " - $" + (.25 * author_earnings).toFixed(2);
+		document.getElementById('report_created').title = new Date(report.created).toDateString();
+		document.getElementById('report_created').innerText = timeago.format(report.created);
+        
+        
+        if (report.children > 0) document.getElementById('comment-sort').style.visibility = "visible";
+        
+        
+		var type = "";
+        
+		if (report.category == "peerquery") type = "writes";
+		if (report.category == "utopian-io") type = "published";
+        
 		const data = {};
+        
+		data.vote_toggle = report.id;
+		data.slider_area = report.id + "-div";
+		
+		data.vote_btn_author = report.author;
+		data.vote_btn_href = report.permlink;
+		data.vote_btn_state = "false";
+		data.vote_btn_id = report.id + "-btn";
+        
+        
+		data.range_id = report.id + "-range";
+		data.votespan_id = report.id + "-percent";
+		
+		data.votes_id = report.id + "-rescount";
+		
+		data.post_voter_main_id = report.id + "-voterlist";
+		
+        
 		data.title = report.title;
 		data.author = report.author;
-		data.author_name = report.author.toUpperCase();
+		data.href = report.permlink;
+        data.author_name = report.author.toUpperCase();
 		data.author_href = "/@" + report.author;
 		data.author_img = "https://steemitimages.com/u/" + report.author + "/avatar";
 		data.category = report.category;
@@ -47,7 +92,7 @@ var dsteem = require('dsteem'),
 		
 		data.active_votes = report.active_votes.length;
 		data.active_comments = report.children;
-		data.earned = Math.max(Number(report.pending_payout_value.split(' ')[0]), Number(report.total_payout_value.split(' ')[0]) + Number(report.curator_payout_value.split(' ')[0])).toFixed();
+		data.earned = author_earnings.split(".")[0];
 		
 		if (author == active_user) {
 			data.follow_btn_class = "ui disabled button";
@@ -64,7 +109,6 @@ var dsteem = require('dsteem'),
 		}
 		
 		
-		
 		//social media button hrefs
 		data.fb = "https://www.facebook.com/sharer/sharer.php?u=" + window.location;
 		data.twitter = "http://twitter.com/share?text=" + document.title + report.title + "&url=" + window.location + "&hashtags=peerquery," + report.category;
@@ -74,30 +118,181 @@ var dsteem = require('dsteem'),
 		data.tumblr = "http://www.tumblr.com/share?v=3&u=" + window.location + "&t=" + document.title;
 		
 		
-			var report_view = await templator.report_view(data);
-			$("#report_view").append(report_view);
+        var report_view = await templator.report_view(data);
+		$("#report_view").append(report_view);
 			
-			$("#loader").hide();
+		$("#loader").hide();
 		
-		var comments = await client.database.call('get_content_replies', [author, permlink]);
+        
+		utils.check_vote(report, report.active_votes);
 		
-		for (var x in comments) {
-			var comment = await creator.comment(comments[x]);
-			$("#comments-container").append(comment);	
-		}
-		
-		
-		
-		
-		$("#share-toggle").on('click', function(){
+        
+        if (report.net_votes > 0) { 
+            document.getElementById(report.id + '-rescount').innerText = utils.count_votes(report.active_votes);
+        } else if (report.net_votes < 0) {
+            document.getElementById('netvotes').className = "angle down icon";
+            document.getElementById(report.id + '-rescount').innerText =  utils.count_votes(report.active_votes);
+        } else if  (report.net_votes == 0) {
+            document.getElementById(report.id + '-rescount').innerText = " 0";
+        }
 	
+        post_category = report.parent_permlink;
+        parent_author = report.author;
+        parent_permlink = report.permlink;
+	
+	
+        
+        
+        function votePanelBtn(eID) {
+            
+            if(!active_user || active_user === ''){ window.location.href = '/login'; return ; }
+            
+            var slidrDiv = document.getElementById(eID + "-div");
+            var slidr = document.getElementById(eID + "-range");
+            
+            $("#" + eID + "-div").slideToggle();
+            
+        };
+    
+	
+
+        function vote(elID) {
+        
+            if (document.getElementById(elID).className.indexOf('disabled') == -1 ){		//this code makes sure that the click only works when the button is NOT disabled
+
+                var vter = active_user;
+                var athor = document.getElementById(elID).dataset.author;
+                var pmlink = document.getElementById(elID).dataset.href;
+                //var weight = Number(document.getElementById(parseInt(elID) + "-range").value);  // not recommended since it removes -/negative values
+                //var weight = parseInt(document.getElementById(parseInt(elID) + "-range").value);
+                var weight = parseInt(document.getElementById(elID).dataset.value);
+                
+                var cClass = document.getElementById(elID).className;
+                var vClass = cClass + " disabled";
+                
+                document.getElementById(elID).className = vClass;  //disable the button while the OP is in progress
+                document.getElementById(parseInt(elID) + "-range").disabled = true;	//disbale the slider once the OP is in progress
+                
+                steem_api.vote(vter, athor, pmlink, weight, function (err, reslt) {
+                    //console.log(err, reslt);
+                    if (err) {
+                        alert(err + "\n" + "Please try again!");
+                        document.getElementById(elID).className = cClass;  //re-enable the btn after OP is done
+                        console.log(err);
+	
+                    } else {
+                        //console.log(reslt)
+                        document.getElementById(elID).className = cClass;  //re-enable the btn after OP is done
+	
+                        if (reslt.result.operations[0][1].weight > 0) {
+                            document.getElementById(elID).dataset.votestate = "true";
+                            //document.getElementById(elID).className = "btn btn-warning btn-sm fa fa-thumbs-down float-right";
+                            document.getElementById(elID).dataset.value = "0";
+                            document.getElementById(elID).innerText = " Unvote";
+                            document.getElementById(parseInt(elID) + "-percent").innerHTML = "<span class='desktop-only'>Upvoted at: </span>" + document.getElementById(parseInt(elID) + "-range").value/100 + "%";
+                            document.getElementById(parseInt(elID) + "-rescount").innerText = " " + (Number(document.getElementById(parseInt(elID) + "-rescount").innerText) + 1);
+                            //document.getElementById(parseInt(elID) + "-range").disabled = true; //disable the slider after a vote is down
+                            console.log("voted successfully");
+                        } else /* if (reslt.result.operations[0][1].weight = 0) */ {		// } else if {... does NOT work
+                            document.getElementById(elID).dataset.votestate = "false";
+                            document.getElementById(elID).dataset.value = "2000";
+                            document.getElementById(parseInt(elID) + "-range").value = "2000";
+                            //document.getElementById(elID).className = "btn btn-success btn-sm fa fa-thumbs-up float-right";
+                            document.getElementById(parseInt(elID) + "-range").disabled = false;	//re-enable the slider when a vote is removed
+                            document.getElementById(parseInt(elID) + "-percent").innerHTML = '<i class="thumbs up icon"></i><span class="desktop-only">Upvote</span>';
+                            document.getElementById(elID).innerText = " +" + document.getElementById(parseInt(elID) + "-range").value/100 + "%";
+                            document.getElementById(parseInt(elID) + "-rescount").innerHTML = " " + (Number(document.getElementById(parseInt(elID) + "-rescount").innerText) + -1);
+                            console.log("vote removed!");
+                        }
+                    }
+                });
+
+            }
+        
+        }
+
+
+
+        
+        //event listeners
+        
+		$("#share-toggle").on('click', function(){
 			$("#share-area").slideToggle("slow");
-			
-		});
-   
+		});        
+        
+		$(".vote_toggle").on('click', function(){
+			votePanelBtn(this.id);
+		});        
+        
+		$(".vote_btn").on('click', function(){
+			vote(this.id);
+		});        
+        
+        $("#response-toggle").click(function(){		
+            if (!active_user || active_user === '') { window.location.href = "/login"; return ; };
+            $("#response-form").slideToggle("slow");	
+        });
+	
+    
+    
+
+        $(".comment_sort").click(async function(){
+            
+            var sort = this.id;
+	
+            document.getElementById('comments-container').innerHTML = "";
+            document.getElementById('comments-spinner').style.display = "block";
+            
+            var comments = await client.database.call('get_content_replies', [author, permlink]);
+            
+            if(sort == "oldest") comments = comments;
+            if(sort == "trending") comments = await comments.sort(function(a, b) { return Number(b.net_rshares) - Number(a.net_rshares)	});
+            if(sort == "newest") comments = await comments.sort(function(a, b){return b.id - a.id});
+            if(sort == "voted") comments = await comments.sort(function(a, b){return b.net_votes - a.net_votes});
+        
+            if (comments.length == 0) { 
+                responsesReady();
+                return;
+            }
+            
+            var comment_active_votes = [];
+            
+            
+            
+            
+            var contributors_earnings = comments.reduce(function(prev, cur) {
+                return prev + Number(Math.max(Number(cur.pending_payout_value.split(' ')[0]), Number(cur.total_payout_value.split(' ')[0]) + Number(cur.curator_payout_value.split(' ')[0])).toFixed(3))
+                //return prev + cur.msgCount;
+            }, 0);
+            
+        
+            for (var x in comments) {
+                comment_active_votes.push( await client.database.call('get_active_votes', [comments[x].author, comments[x].permlink]) );
+            }
+            
+            document.getElementById('contributors_earnings').innerText = "$" + contributors_earnings.toFixed(2) + " + $" + (.25 * author_earnings).toFixed(2);
+            document.getElementById('responsescount').innerText = comments.length.toLocaleString();
+            document.getElementById('comments-spinner').style.display = "none";
+            
+            for (var x in comments) {
+            
+                var comment = await creator.comment(comments[x]);
+                $("#comments-container").append(comment);
+            
+                utils.check_response_votes(comments[x], comments[x].net_votes, comment_active_votes[x]);
+            
+            }
+            
+            
+            responsesReady();
+            
+            
+        });
+        
+        
+        document.getElementById('voted').click();
 		
-		
-		
+        
 	} catch(err){
 		console.log(err);
 	}
