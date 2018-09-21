94648d9d0f84144c907e0dddf509b061b955f244
dzivenu <makafuigdzivenu@gmail.com>
2018-09-21 00:38:19 +0000

    add query page builder

---
+++ src/client/pages/query.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+var parser = require('../../lib/post-parser-web');
+
+	//jquery is already universal through the `ui.js` global file
+    
+    
+(async () => {
+	
+	try {
+        
+        $("#body").html(parser.content( $("#temp").html() ));
+        $("#temp").remove();
+        
+	} catch(err){
+		console.log(err);
+	}
+    
+    
+})();
+
+
