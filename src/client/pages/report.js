fb5b1772b242e350f0c4248c8cc99ddb5d66623b
dzivenu <makafuigdzivenu@gmail.com>
2018-09-22 21:31:39 +0000

    fix vote button event listener

--- src/client/pages/report.js
+++ src/client/pages/report.js
@@ -318,10 +318,10 @@ $( window ).on( 'load', async function() {
         votePanelBtn(this.id);
     });   
         
-    $('.vote_btn').on('click', function(){
+    $('#comments-container').on('click', '.vote_btn', function(){
         vote(this.id);
-    });        
-        
+    });
+    
     $('#response-toggle').click(function(){		
         if (!active_user || active_user === '') { window.location.href = '/login'; return ; }
         $('#response-form').slideToggle('slow');	
