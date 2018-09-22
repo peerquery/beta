3b3a32341ad7cc853ca3817994ebb9cb669091e3
dzivenu <makafuigdzivenu@gmail.com>
2018-09-22 17:04:08 +0000

    re-organize for modularity

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
 
 'use strict';
 	
-const init = async () => {
-	
-    require('dotenv').config();
-	
-    const mongoose = require('./configs/mongoose'),
-        path = require('path'),
-        favicon = require('serve-favicon'),
-        express = require('./server/server');
+require('dotenv').config();
+    
+const mongoose = require('./configs/mongoose'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    express = require('./server/server');
     //start = require('./server/init')();
     
-    try {
-        var db = mongoose();
-        var server = express();
-    } catch (err){
-        console.log(err);
-    }	
+try {
+    var db = mongoose();
+    var app = express();
+} catch (err){
+    console.log(err);
+}	
 	
-    server.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
     
-    const node_env = process.env.NODE_ENV,
-        server_port = Number(process.env.SERVER_PORT) || 80,
-        port = (node_env == 'production') ? server_port + 8001 : server_port;
-        
-    server.listen(port);
-    //start();
+const node_env = process.env.NODE_ENV,
+    server_port = Number(process.env.SERVER_PORT),
+    port = (node_env == 'production') ? server_port + 8001 : server_port;
+    
+app.listen(port);
+//start();
 	
-    console.log('\n\n\nServer running at port: ' + port);
+console.log('\n\n\nServer running at port: ' + port);
 	
-};
-
-init();
-
-module.exports = init;
+module.exports = app;
