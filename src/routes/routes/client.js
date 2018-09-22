5a965cdd09a641027a7d1f226d1b80ba415fae98
dzivenu <makafuigdzivenu@gmail.com>
2018-09-22 16:40:03 +0000

    restrict access

--- src/routes/routes/client.js
+++ src/routes/routes/client.js
@@ -3,26 +3,25 @@
 
 var router = require('../../server/router'),
     address = require('../indexes/address'),
+    authorize = require('../auth/authorize'),
     cookie = require('cookie-parser');
 	
 module.exports = function(app) {
 	
     app.get('/login', function (req, res) {
-		
         if (req.active_user) return res.redirect('/@' + req.active_user.account);
         return router(address.client.login, req, res);
-		
     });
 	
-    app.get('/create/report', function (req, res) {
+    app.get('/create/report', authorize, function (req, res) {
         return router(address.client.new_report, req, res);
     });
 	
-    app.get('/create/query', function (req, res) {
+    app.get('/create/query', authorize, function (req, res) {
         return router(address.client.new_query, req, res);
     });
 	
-    app.get('/create/project', function (req, res) {
+    app.get('/create/project', authorize, function (req, res) {
         return router(address.client.new_project, req, res);
     });
 	
