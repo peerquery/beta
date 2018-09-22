2a2ae2a25e1cfd034f4571bcb92441906cb158d3
dzivenu <makafuigdzivenu@gmail.com>
2018-09-22 17:05:09 +0000

    activate test api file

--- src/apis/common.js
+++ src/apis/common.js
@@ -1,11 +1,13 @@
 
 'use strict';
 
-var content = require('./content');
+var content = require('./content'),
+    test = require('./test');
 	
 module.exports = async function (app) {
 	
     content(app);
+    test(app);
 	
 };
 	
