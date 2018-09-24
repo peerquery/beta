caeb80e190b35ffcc25570bdde8254141c0a1392
dzivenu <makafuigdzivenu@gmail.com>
2018-09-24 18:52:25 +0000

    attempt to discourage staging indexing

--- src/routes/helpers/robots.js
+++ src/routes/helpers/robots.js
@@ -3,12 +3,19 @@
 module.exports = function(app) {
 
     app.get('/robots.txt', function (req, res) {
+        var data;
+        if(req.subdomains.indexOf('staging') > -1) {    //staging deployment
+            data = 
+                'User-agent: *' +
+                '\nDisallow: /';
+        } else {                                         //production deployment
+            data = 
+                'User-agent: *' +
+                '\nDisallow: /api*' +
+                '\nSitemap: https://www.peerquery.com/sitemap.xml';
+        }
         res.type('text/plain');
-        res.send(
-            'User-agent: *' +
-			'\nDisallow: /api*' +
-			'\nSitemap: https://www.peerquery.com/sitemap.xml'
-        );
+        res.send(data);
     });
 
 };
