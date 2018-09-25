14d83bb9c8cded5b3f77647a0ba35b626469836b
dzivenu <makafuigdzivenu@gmail.com>
2018-09-25 13:06:09 +0000

    chain notifications-container click listeners

--- src/client/ui/listeners.js
+++ src/client/ui/listeners.js
@@ -6,15 +6,16 @@ module.exports = function() {
         $('html, body').animate({scrollTop: '+=600px'}, 800);
     });
 	
-    $('#notifications-container').on('click', '.cookie_consent', function(event){
-        $(this).hide();
-        var d = new Date();
-        d.setTime(d.getTime() + ( 7 * 24 * 60 * 60 * 1000 )); //7 days
-        var expires = 'expires=' + d.toUTCString();
-        document.cookie = 'cookie_reminder' + '=' + new Date().toDateString() + ';' + expires + ';path=/';
-    });
-	
-    //listener for notifications message close icon click
-    $('#notifications-container').on('click', '.message .close', function() { $(this).closest('.message').transition('fade'); });
-	
+    $('#notifications-container')
+        .on('click', '.cookie_consent', function(event){
+            $(this).hide();
+            var d = new Date();
+            d.setTime(d.getTime() + ( 7 * 24 * 60 * 60 * 1000 )); //7 days
+            var expires = 'expires=' + d.toUTCString();
+            document.cookie = 'cookie_reminder' + '=' + new Date().toDateString() + ';' + expires + ';path=/';
+        })
+        .on('click', '.message .close', function() {//listener for notifications message close icon click
+            $(this).closest('.message').transition('fade');
+        });
+        
 };
