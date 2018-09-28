1d227fe74952ced34edd81cf7d2ea4524f5643d3
dzivenu <makafuigdzivenu@gmail.com>
2018-09-28 11:46:08 +0000

    remove duplicate listener

--- src/client/ui/semantic_ui.js
+++ src/client/ui/semantic_ui.js
@@ -22,30 +22,4 @@ module.exports = function() {
     });
 
     $('.ui.dropdown').dropdown();
-
-    //------------post preview popup system
-
-    $('#item-container').on('click', '.pop', function() {
-        var el = this;
-
-        jQuery('.ui.modal').modal('show');
-
-        $('.ui.modal')
-            .modal({
-                onShow: function() {
-                    //window.pqy_notify.warn('Showing modal');
-                    //console.log(this); //works but rather only logs the real modal itself!
-                    document.getElementById('modal_href').href =
-                        '/@' + el.dataset.account + '/' + el.dataset.permlink;
-                    document.getElementById('modal_title').innerHTML =
-                        el.dataset.title;
-                    document.getElementById('modal_author').innerHTML =
-                        '@' + el.dataset.account;
-                    document.getElementById('modal_content').innerHTML =
-                        el.dataset.body;
-                    return false;
-                },
-            })
-            .modal('show');
-    });
 };
