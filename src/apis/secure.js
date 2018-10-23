3896b5101f9612b50687f97f1e5c69677774ab0b
dzivenu <makafuigdzivenu@gmail.com>
2018-10-23 13:10:13 +0000

    activate project manage/settings modules

--- src/apis/secure.js
+++ src/apis/secure.js
@@ -2,10 +2,14 @@
 
 var auth = require('./auth'),
     create = require('./create'),
+    manage = require('./manage'),
+    settings = require('./settings'),
     update = require('./update');
 
 module.exports = async function(app) {
     auth(app);
     create(app);
+    manage(app);
+    settings(app);
     update(app);
 };
