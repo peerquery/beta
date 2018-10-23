fa2920c005fe5bb824be93b8c584057c5dcbab6d
dzivenu <makafuigdzivenu@gmail.com>
2018-10-23 12:44:10 +0000

    include manage page controller

---
+++ src/apis/manage.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    project = require('../models/project');
+//do not worry about sanitizing req.body; already done in the server!
+
+module.exports = function(app) {
+    app.post('/api/private/update/project/action_button', async function(
+        req,
+        res
+    ) {
+        try {
+            var query = {
+                slug_id: req.body.slug_id,
+                owner: req.active_user.account,
+            };
+
+            var update = {
+                act_msg: req.body.msg,
+                act_uri: req.body.uri,
+            };
+
+            var _status = await project.updateOne(query, update, {
+                upsert: true,
+            });
+
+            if (!_status) return res.sendStatus(500);
+
+            res.sendStatus(200);
+        } catch (err) {
+            res.sendStatus(500);
+            console.log(err);
+        }
+    });
+};
