6b04df26ca2b1570d426b225b200115769bf1819
dzivenu <makafuigdzivenu@gmail.com>
2018-10-25 23:45:33 +0000

    improve data renderer

--- src/client/pages/partials/project-stats.js
+++ src/client/pages/partials/project-stats.js
@@ -7,15 +7,32 @@ $(window).on('load', function() {
                 $.get('/api/project/' + project_slug + '/stats')
             );
 
+            let report_count = response.overview
+                ? response.overview.report_count || 0
+                : 0;
+            let query_count = response.overview
+                ? response.overview.query_count || 0
+                : 0;
+
+            let member_count = response.overview
+                ? response.overview.member_count || 0
+                : 0;
+            let pending_count = response.pending[0]
+                ? response.pending[0].pending_count || 0
+                : 0;
+            let team_count = response.team[0]
+                ? response.team[0].team_count || 0
+                : 0;
+
             //overview
-            $('#report_count').text(response.overview.report_count);
-            $('#query_count').text(response.overview.query_count);
+            $('#report_count').text(report_count);
+            $('#query_count').text(query_count);
             $('#activity-section').show();
 
             //community
-            $('#member_count').text(response.overview.member_count);
-            $('#pending_count').text(response.pending[0].pending_count);
-            $('#team_count').text(response.team[0].team_count);
+            $('#member_count').text(member_count);
+            $('#pending_count').text(pending_count);
+            $('#team_count').text(team_count);
             $('#community-section').show();
 
             $('#stats-loader').hide();
