09662ad242ee832cbd67455f1091a318ac134325
dzivenu <makafuigdzivenu@gmail.com>
2018-10-25 23:36:57 +0000

    refactor for new settings page

--- src/client/pages/partials/project-settings.js
+++ src/client/pages/partials/project-settings.js
@@ -1,160 +1,97 @@
+var downloader = require('./../../../lib/helpers/downloader');
+
 $(window).on('load', function() {
-    var Editor = require('../../../lib/editor'),
-        config = require('../../../configs/config');
-
-    window.Editor = Editor;
-
-    Editor.disable_image_upload();
-
     //jquery is already universal through the `scripts.js` global file
 
-    $('#updateBtn').on('click', function() {
-        if (validateForm() == true) {
-            post();
-            $('form').addClass('loading');
-            $('#' + this.id).addClass('disabled');
-        }
-    });
-
-    var data = {};
-
-    function validateForm() {
-        data.slug = project_slug;
-        data.name = $('#projectName').val();
-        data.location = $('#projectLocation').val();
-        //data.founder = active_user; //handled server side for thorough consistency
-        data.logo = $('#projectLogo').val();
-        data.cover = $('#projectCover').val();
-        data.description = $('#projectDescription')
-            .val()
-            .substring(0, 160);
-        data.mission = $('#projectMission')
-            .val()
-            .substring(0, 160);
-        data.story = $('<div />')
-            .html(Editor.setup.getMarkdown())
-            .find('span')
-            .contents()
-            .unwrap()
-            .end()
-            .end()
-            .html();
-        //data.state = $('#projectState').dropdown('get value');
-        data.state = $('#projectState')
-            .find(':selected')
-            .text();
-        data.tag = $('#projectTag').val();
-        data.website = $('#projectWebsite').val();
-        data.website =
-            data.website.indexOf('://') === -1
-                ? 'http://' + data.website
-                : data.website;
-        data.steem = $('#projectSteem').val();
-        data.facebook = $('#projectFacebook').val();
-        data.twitter = $('#projectTwitter').val();
-        data.github = $('#projectGithub').val();
-        data.chat = $('#projectChat').val();
-
-        var required = [data.name, data.description, data.state, data.tag];
-        var empty = '';
-        var invalid = required.indexOf(empty);
-
-        if (invalid > -1) {
-            var name = '';
-            var id = '';
-            var field = '';
-
-            if (invalid == 0) {
-                name = 'Name field';
-                (id = 'projectName'), (field = 'nameField');
-            }
-            if (invalid == 1) {
-                name = 'Description field';
-                (id = 'projectField'), (field = 'descriptionField');
-            }
-            if (invalid == 2) {
-                name = 'State field';
-                (id = 'projectState'), (field = 'stateField');
-            }
-            if (invalid == 3) {
-                name = 'Tag field';
-                (id = 'projectTag'), (field = 'tagField');
-            }
-
-            window.pqy_notify.warn(name + ' cannot be empty');
-            $('#' + id).focus();
-            $('#' + field).addClass('error');
-
-            return false;
-        } else if (data.story == '') {
-            //cannot enter empty body
-            window.pqy_notify.warn('Please enter story of your project.');
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    async function post() {
+    async function export_data(type) {
         try {
-            var status = await Promise.resolve(
-                $.post('/api/private/update/project', data)
-            );
-            //console.log(status);
-            window.location.href = '/project/' + status;
-        } catch (err) {
-            console.log(err);
-            window.pqy_notify.warn('Sorry, an error occured. Please again');
-            window.location.reload();
-        }
-    }
+            window.pqy_notify.inform('Fetching data, please wait');
 
-    $('.limitedText').on('keyup', function() {
-        var maxLength = $(this).attr('maxlength');
-        if (maxLength == $(this).val().length) {
-            window.pqy_notify.warn(
-                'You can\'t write more than ' + maxLength + ' characters'
-            );
-        }
-    });
+            let data = {
+                slug_id: project_slug,
+                type: type,
+            };
 
-    (async () => {
-        try {
             var response = await Promise.resolve(
-                $.get('/api/private/project/' + project_slug + '/settings')
+                $.post('/api/private/project/export', data)
             );
-            $('#settings-loader').hide();
-            $('#settings-form').show();
-            $('#updateBtn').show();
 
-            response = response[0];
+            if (!response)
+                return window.pqy_notify.warn(
+                    'Sorry, an error occured. Please again'
+                );
 
-            $('#projectName').val(response.name);
-            $('#projectLocation').val(response.location);
-            $('#projectLogo').val(response.logo);
-            $('#projectCover').val(response.cover);
-            $('#projectDescription').val(response.description);
-            $('#projectMission').val(response.mission);
-            //$('#edito').val(response.story);
+            window.pqy_notify.inform(
+                'Successfully fetched data, now downloading'
+            );
 
-            Editor.setup.setHtml(Editor.setup.convertor.toHTML(response.story));
-
-            $('#projectState')
-                .val(response.state.toLowerCase())
-                .change();
-            $('#projectTag').val(response.tag);
-            $('#projectWebsite').val(response.website);
-            $('#projectSteem').val(response.steem);
-            $('#projectFacebook').val(response.facebook);
-            $('#projectTwitter').val(response.twitter);
-            $('#projectGithub').val(response.github);
-            $('#projectChat').val(response.chat);
-
-            //console.log(response);
+            downloader.json(response, slug_id);
         } catch (err) {
             console.log(err);
             window.pqy_notify.warn('Sorry, an error occured. Please again');
             window.location.reload();
         }
-    })();
+    }
+
+    $('#json').on('click', function() {
+        $(this).addClass('disabled');
+        export_data('json');
+    });
+
+    $('#xml').on('click', function() {
+        $(this).addClass('disabled');
+        export_data('xml');
+    });
+
+    $('#authorize_transfer').on('click', async function() {
+        try {
+            $(this).addClass('disabled');
+
+            //let new_owner = $('#teamField').dropdown('get value');
+
+            let new_owner = $('#input').val();
+            let data = {
+                new_owner: new_owner,
+                slug_id: slug_id,
+            };
+
+            window.pqy_notify.inform('Tranfering project, please wait');
+
+            var response = await Promise.resolve(
+                $.post('/api/private/project/transfer', data)
+            );
+
+            window.pqy_notify.inform('Successfully tranfered project');
+
+            window.location.href = '/project/' + project_slug;
+        } catch (err) {
+            console.log(err);
+            window.pqy_notify.warn('Sorry, an error occured. Please again');
+            //window.location.reload();
+        }
+    });
+
+    $('#authorize_delete').on('click', async function() {
+        try {
+            $(this).addClass('disabled');
+
+            window.pqy_notify.inform('Deleting project, please wait');
+            let data = {
+                title: window.project_title,
+                slug_id: slug_id,
+            };
+
+            var response = await Promise.resolve(
+                $.post('/api/private/project/delete', data)
+            );
+
+            window.pqy_notify.inform('Successfully deleted project');
+
+            window.location.href = '/projects';
+        } catch (err) {
+            console.log(err);
+            window.pqy_notify.warn('Sorry, an error occured. Please again');
+            //window.location.reload();
+        }
+    });
 });
