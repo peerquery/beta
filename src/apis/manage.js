198ed99b19a17468b8ee83fff3fd7c70e6fd3dac
dzivenu <makafuigdzivenu@gmail.com>
2018-11-05 18:33:49 +0000

    include controller for theme update

--- src/apis/manage.js
+++ src/apis/manage.js
@@ -5,6 +5,37 @@ var mongoose = require('mongoose'),
 //do not worry about sanitizing req.body; already done in the server!
 
 module.exports = function(app) {
+    app.post('/api/private/update/project/project_theme', async function(
+        req,
+        res
+    ) {
+        try {
+            var query = {
+                slug_id: req.body.slug_id,
+                owner: req.active_user.account,
+            };
+
+            var update = {
+                $set: {
+                    type: req.body.type,
+                    color: req.body.color,
+                },
+            };
+
+            var _status = await project.updateOne(query, update, {
+                upsert: true,
+                setDefaultsOnInsert: true,
+            });
+
+            if (!_status) return res.sendStatus(500);
+
+            res.sendStatus(200);
+        } catch (err) {
+            res.sendStatus(500);
+            console.log(err);
+        }
+    });
+
     app.post('/api/private/update/project/action_button', async function(
         req,
         res
@@ -16,12 +47,15 @@ module.exports = function(app) {
             };
 
             var update = {
-                act_msg: req.body.msg,
-                act_uri: req.body.uri,
+                $set: {
+                    act_msg: req.body.msg,
+                    act_uri: req.body.uri,
+                },
             };
 
             var _status = await project.updateOne(query, update, {
                 upsert: true,
+                setDefaultsOnInsert: true,
             });
 
             if (!_status) return res.sendStatus(500);
