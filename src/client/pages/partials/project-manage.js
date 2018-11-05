1d3084a9cf93cdba41779d61435d62cd5abeb9d0
dzivenu <makafuigdzivenu@gmail.com>
2018-11-05 18:45:13 +0000

    set up update function

--- src/client/pages/partials/project-manage.js
+++ src/client/pages/partials/project-manage.js
@@ -1,6 +1,31 @@
 $(window).on('load', function() {
     //jquery is already universal through the `scripts.js` global file
 
+    $('#update_project_theme').on('click', async function() {
+        try {
+            $(this).addClass('disabled');
+            window.pqy_notify.inform('Updating project, please wait');
+
+            let data = {
+                slug_id: slug_id,
+                type: $('#projectType')
+                    .find(':selected')
+                    .val(),
+                color: $('#projectColor').val(),
+            };
+
+            var response = await Promise.resolve(
+                $.post('/api/private/update/project/project_theme', data)
+            );
+
+            window.pqy_notify.success('Successfully updated project theme');
+        } catch (err) {
+            console.log(err);
+            window.pqy_notify.warn('Sorry, an error occured. Please again');
+            //window.location.reload();
+        }
+    });
+
     $('#update_action_button').on('click', async function() {
         try {
             $(this).addClass('disabled');
