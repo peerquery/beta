c5b7f2f22f1672d72cbdb6b635834f418f6f261b
dzivenu <makafuigdzivenu@gmail.com>
2018-11-09 16:06:39 +0000

    mount office controller

--- src/apis/secure.js
+++ src/apis/secure.js
@@ -2,6 +2,7 @@
 
 var auth = require('./auth'),
     create = require('./create'),
+    office = require('./office'),
     manage = require('./manage'),
     settings = require('./settings'),
     update = require('./update');
@@ -10,6 +11,7 @@ module.exports = async function(app) {
     auth(app);
     create(app);
     manage(app);
+    office(app);
     settings(app);
     update(app);
 };
