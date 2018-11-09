6b9982cef397a5edec73ebf7830747072fd2b8ec
dzivenu <makafuigdzivenu@gmail.com>
2018-11-09 16:05:02 +0000

    update from capped to uniqued, include essential fields

--- src/models/settings.js
+++ src/models/settings.js
@@ -3,19 +3,36 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var settingsSchema = new Schema(
-    {
-        admin_account: { type: String, unique: true, required: true },
-        admin_email: { type: String, unique: true, required: true },
-        admin_hash: { type: String, required: true },
-        init_time: { type: Date, default: Date.now, required: true },
-        updated_at: Date,
-    },
-    {
-        //has to be a single document
-        collection: 'settingsSchema',
-        capped: { size: 1024, max: 1 },
-    }
-);
+var settingsSchema = new Schema({
+    //static id - to ensure there is always only one 'settings' document
+    identifier: { type: String, unique: true, required: true },
+
+    //meta
+    name: String,
+    description: String,
+    account: { type: String, required: true },
+    email: { type: String },
+
+    //bot rates
+    community: Number,
+    team: Number,
+    project: Number,
+
+    //bot configs
+    bot_vote_interval: Number,
+    bot_comment_after_vote: Boolean,
+    bot_rest_day: String,
+    bot_key: String,
+
+    //basic team
+    owner: String,
+    admin: String,
+
+    //uris
+    domain: String,
+    steem: String,
+
+    last_update: Date,
+});
 
 module.exports = mongoose.model('settingsSchema', settingsSchema);
