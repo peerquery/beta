e059e04e0e25c227d0448d94073ab98daba6de88
dzivenu <makafuigdzivenu@gmail.com>
2018-11-10 22:54:45 +0000

    add silent debug config

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
     mongoose.Promise = global.Promise;
+
+    //mongoose.set('debug', true);
+
     var db = mongoose.connect(
         process.env.MONGODB_URI,
         { useNewUrlParser: true /*, autoIndex: false */ }
