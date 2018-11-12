ed363ba9556d32cfec33219bbbcf446e6951427d
dzivenu <makafuigdzivenu@gmail.com>
2018-11-12 22:30:48 +0000

    mount office router

--- src/routes/indexes/secure.js
+++ src/routes/indexes/secure.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var client = require('../routes/client'),
-    content = require('../routes/content');
+    content = require('../routes/content'),
+    office = require('../routes/office');
 
 module.exports = async function(app) {
     client(app);
+    office(app);
     content(app); //always mount this router last, else **/@:username/:permLink** will override some of the routes handled above
 };
