955e1b952e86c2bef06cc5fe863631222bddf88d
dzivenu <makafuigdzivenu@gmail.com>
2018-11-12 22:28:51 +0000

    include curation details

--- src/configs/config.js
+++ src/configs/config.js
@@ -8,11 +8,16 @@ var config = {};
 config.site_name = 'Peer Query';
 config.site_description = 'Peer to Peer collaborations powered by Steem';
 config.site_uri = 'http://www.peerquery.com';
+config.super_admin = 'dzivenu';
 
 //steem
 config.steem_api = 'https://api.steemit.com';
 config.steem_rpc = 'https://api.steemit.com';
 config.steem_account = 'peerquery';
+config.community = 'peerquery';
+
+//bot account
+config.bot_account = 'peerquery';
 
 //sc2
 config.sc2_app_name = 'peerquery.app';
@@ -24,5 +29,18 @@ config.report_attribution =
     '  \n\n- - -\n\n  Published on [Peer Query - Blockchain-powered p2p collaboration](URL).  \n\n  ';
 config.comment_attribution =
     '  \n\n- - -\n\n  Posted on [Peer Query](http://www.peerquery.com).';
+
+//curation
+config.curation_curator_rate = 2;
+config.curation_project_rate = 20;
+config.curation_team_rate = 15;
+config.curation_community_rate = 5;
+
+config.curation_daily_limit = 20;
+config.curation_rest_day1 = 'saturday';
+config.curation_rest_day2 = 'sunday';
+config.curation_vote_interval_minutes = 20;
+config.curation_common_comment =
+    'Nice work! Consider delegating Steem power to support Peer Query in its bit to support peer to peer collaborations.';
 
 module.exports = config;
