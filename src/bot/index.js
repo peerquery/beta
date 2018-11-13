bea09c0bd5e21d42182ba84c4adc50cdbfcbe362
dzivenu <makafuigdzivenu@gmail.com>
2018-11-13 22:45:16 +0000

    update globals getter call methods

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const global_config = require('./globals');
-//console.log(global_config());
+//console.log(await global_config());
 
 const voter_bot = require('./voter-bot');
 const curation_voter = require('./curation-voter');
@@ -12,9 +12,10 @@ const today = require('../lib/helpers/day');
 module.exports = async function(app) {
     //main timer which only runs every 24 hours and calls or shuts(on bot rest day) all other timers
     setInterval(async function() {
+        var config = await global_config();
         if (
-            today.toLowerCase() == (await global_config().curation_rest_day1) ||
-            today.toLowerCase() == (await global_config().curation_rest_day2)
+            today.toLowerCase() == config.curation_rest_day1 ||
+            today.toLowerCase() == config.curation_rest_day2
         ) {
             clearInterval(curation_voter_timer);
         } else {
@@ -24,8 +25,8 @@ module.exports = async function(app) {
                 curation_voter_timer();
 
                 //these are not timers, just functions that run every 24 hours
-                voter_bot(curator_voter(await global_config()));
-                voter_bot(team_project_voter(await global_config()));
+                voter_bot(curator_voter(config));
+                voter_bot(team_project_voter(config));
             } catch (err) {
                 console.log(err);
             }
@@ -33,8 +34,13 @@ module.exports = async function(app) {
     }, 1440 * 60 * 1000); //24 hours
 
     //vote curation
-    var curation_interval =
-        (await global_config().curation_vote_interval_minutes) * 60 * 60 * 1000;
+
+    var get_curation_interval = async () => {
+        var globals = await global_config();
+        return globals.curation_vote_interval_minutes;
+    };
+
+    var curation_interval = (await get_curation_interval()) * 60 * 60 * 1000;
     var curation_voter_timer = setInterval(async function() {
         try {
             voter_bot(curation_voter(await global_config()));
