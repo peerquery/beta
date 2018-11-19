c03d221543308e829b718bbc17528613a4b6fc25
dzivenu <makafuigdzivenu@gmail.com>
2018-11-19 10:24:53 +0000

    include static routers

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,6 +4,7 @@ var auth = require('./auth/index'),
     content = require('./content/index'),
     create = require('./create/index'),
     manage = require('./manage/index'),
+    _static = require('./static/index'),
     office = require('./office/index');
 
 module.exports = function(app) {
@@ -11,5 +12,6 @@ module.exports = function(app) {
     content(app);
     create(app);
     manage(app);
+    _static(app);
     office(app);
 };
