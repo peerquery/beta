0756681de497f1518d2bf4aa28a8280e4f01df33
dzivenu <makafuigdzivenu@gmail.com>
2018-11-21 12:04:07 +0000

    set up static page routers

--- src/routes/routes/static.js
+++ src/routes/routes/static.js
@@ -1,23 +1,70 @@
 'use strict';
 
-var router = require('../../server/router'),
+var stats = require('../../models/stats'),
+    router = require('../../server/router'),
     address = require('../indexes/address');
 
 module.exports = function(app) {
+    app.get('/about', async function(req, res) {
+        try {
+            var query =
+                'peer_count project_count curation_worth request_count -_id';
+
+            var results = await stats
+                .findOne({ identifier: 'default' })
+                .select(query);
+
+            if (!results || results == '')
+                return router(address._static._404, req, res);
+
+            res.req_data = results;
+            return router(address._static.about, req, res);
+        } catch (err) {
+            console.log(err);
+            return router(address._static._500, req, res);
+        }
+
+        //return router(address._static.about, req, res);
+    });
+
     app.get('/create', function(req, res) {
         return router(address._static.create, req, res);
     });
 
-    app.get('/support/contact', function(req, res) {
-        return router(address._static.contact, req, res);
+    app.get('/curation', function(req, res) {
+        return router(address._static.curation, req, res);
+    });
+
+    app.get('/statistics', function(req, res) {
+        return router(address._static.statistics, req, res);
     });
 
     app.get('/abuse', function(req, res) {
         return router(address._static.abuse, req, res);
     });
 
+    app.get('/partners', function(req, res) {
+        return router(address._static.partners, req, res);
+    });
+
+    app.get('/blog', function(req, res) {
+        return router(address._static.blog, req, res);
+    });
+
+    app.get('/support', function(req, res) {
+        return router(address._static.support, req, res);
+    });
+
+    app.get('/support/contact', function(req, res) {
+        return router(address._static.contact, req, res);
+    });
+
     app.get('/support/faqs', function(req, res) {
         return router(address._static.faqs, req, res);
+    });
+
+    app.get('/open-source', function(req, res) {
+        return router(address._static.opensource, req, res);
     });
 
     app.get('/team', function(req, res) {
