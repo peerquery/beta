68ba7317ebc87d21c3714d31d95b261d5134f0b2
dzivenu <makafuigdzivenu@gmail.com>
2018-11-21 12:01:08 +0000

    mount blog/curation controllers

--- src/apis/static/index.js
+++ src/apis/static/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var statistics = require('./statistics'),
-    search = require('./search');
+    search = require('./search'),
+    blog = require('./blog'),
+    curation = require('./curation');
 
 module.exports = function(app) {
     search(app);
     statistics(app);
+    blog(app);
+    curation(app);
 };
