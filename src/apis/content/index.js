185ae32efc95b4ce351e1286725a85613cf2dad8
dzivenu <makafuigdzivenu@gmail.com>
2018-11-29 08:50:21 +0000

    mount messages api

--- src/apis/content/index.js
+++ src/apis/content/index.js
@@ -3,11 +3,13 @@
 var reports = require('./reports'),
     queries = require('./queries'),
     projects = require('./projects'),
-    peers = require('./peers');
+    peers = require('./peers'),
+    messages = require('./messages');
 
 module.exports = function(app) {
     reports(app);
     queries(app);
     projects(app);
     peers(app);
+    messages(app);
 };
