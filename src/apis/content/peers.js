3f35f3832d31640ffb4a91ebab785d1551870447
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 05:14:12 +0000

    re-structure to meet need view design

--- src/apis/content/peers.js
+++ src/apis/content/peers.js
@@ -129,92 +129,89 @@ module.exports = async function(app) {
         }
     });
 
-    app.get('/api/fetch/peers/builders/:last_id', async function(req, res) {
+    app.get('/api/fetch/peers/featured/:last_id', async function(req, res) {
         try {
+            let query =
+                'account about last_project_slug_id last_project_title badge position company skill';
+            let find;
+
             if (req.params.last_id == 0) {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({ project_count: { $gt: 0 } })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {
+                    project_count: { $gt: 0 },
+                    curation_points: { $gt: 0 },
+                };
             } else {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({
-                        project_count: { $gt: 0 },
-                        _id: { $gt: req.params.last_id },
-                    })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {
+                    project_count: { $gt: 0 },
+                    curation_points: { $gt: 0 },
+                    _id: { $gt: req.params.last_id },
+                };
             }
+
+            let results = await peers
+                .find(find)
+                .select(query)
+                .limit(20)
+                .sort({ created: -1 });
+
+            res.status(200).json(results);
         } catch (err) {
             console.log(err);
             res.sendStatus(500);
         }
     });
 
-    app.get('/api/fetch/peers/reporters/:last_id', async function(req, res) {
+    app.get('/api/fetch/peers/interesting/:last_id', async function(req, res) {
         try {
+            let query =
+                'account about last_project_slug_id last_project_title badge position company skill';
+            let find;
+
             if (req.params.last_id == 0) {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({ project_count: 0, report_count: { $gt: 0 } })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {
+                    curation_points: { $gt: 0 },
+                };
             } else {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({
-                        project_count: 0,
-                        report_count: { $gt: 0 },
-                        _id: { $gt: req.params.last_id },
-                    })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {
+                    curation_points: { $gt: 0 },
+                    _id: { $gt: req.params.last_id },
+                };
             }
+
+            let results = await peers
+                .find(find)
+                .select(query)
+                .limit(20)
+                .sort({ report_count: -1 });
+
+            res.status(200).json(results);
         } catch (err) {
             console.log(err);
             res.sendStatus(500);
         }
     });
 
-    app.get('/api/fetch/peers/observers/:last_id', async function(req, res) {
+    app.get('/api/fetch/peers/new/:last_id', async function(req, res) {
         try {
+            let query =
+                'account about last_project_slug_id last_project_title badge position company skill';
+            let find;
+
             if (req.params.last_id == 0) {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({ project_count: 0, report_count: 0 })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {};
             } else {
-                let query =
-                    'account about last_project_slug_id last_project_title badge created state';
-                let results = await peers
-                    .find({
-                        project_count: 0,
-                        report_count: 0,
-                        _id: { $gt: req.params.last_id },
-                    })
-                    .select(query)
-                    .limit(20)
-                    .sort({ created: -1 });
-                res.status(200).json(results);
+                find = {
+                    _id: { $gt: req.params.last_id },
+                };
             }
+
+            let results = await peers
+                .find(find)
+                .select(query)
+                .limit(20)
+                .sort({ _id: -1 });
+
+            res.status(200).json(results);
         } catch (err) {
             console.log(err);
             res.sendStatus(500);
