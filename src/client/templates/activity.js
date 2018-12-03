53be60af241c11df16344e35bf36ae1792ee7417
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 20:34:54 +0000

    include activity template

---
+++ src/client/templates/activity.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var activity =
+    '     ' +
+    '                   <div class="content">  ' +
+    '                       <div class="summary">  ' +
+    '                           <i class="{{icon}} icon"></i>  ' +
+    '                           {{message}}  ' +
+    '                           <div class="date">  ' +
+    '                               {{created}}  ' +
+    '                           </div>  ' +
+    '                       </div>  ' +
+    '                   </div>  ';
+
+module.exports = activity;
