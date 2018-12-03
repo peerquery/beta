aa0504548911b63f3dcf610dd4cd0e1678565de2
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 23:42:13 +0000

    load notifications templates only if default message does not exist

--- src/client/pages/account-notifications.js
+++ src/client/pages/account-notifications.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var templator = require('../../client/templator'),
-    messages = require('../../configs/event-messages'),
+    templates = require('../../configs/event-message-templates'),
+    mtools = require('markup-tools'),
     timeago = require('timeago.js')();
 
 //jquery is already universal through the `ui.js` global file
@@ -36,7 +37,17 @@ async function display() {
 
         for (var x in notifications) {
             notifications[x].created = timeago.format(notifications[x].created);
-            notifications[x].description = messages[notifications[x].event];
+
+            if (!notifications[x].messsage) {
+                let template = templates[notifications[x].event];
+                notifications[x].messsage = mtools.build.template(
+                    template(notifications[x])
+                );
+            }
+
+            notifications[x].messsage = mtools.build.mentions(
+                notifications[x].message
+            );
 
             if (notifications[x].source == 'user')
                 notifications[x].graphics =
