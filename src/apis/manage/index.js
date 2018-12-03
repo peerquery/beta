2b7dbd8f359b79da14b94462d3b2f9ffb1174c23
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 23:17:45 +0000

    mount account api

--- src/apis/manage/index.js
+++ src/apis/manage/index.js
@@ -3,11 +3,13 @@
 var manage = require('./manage'),
     settings = require('./settings'),
     update = require('./update'),
+    account = require('./account'),
     _status = require('./status');
 
 module.exports = function(app) {
     manage(app);
     settings(app);
     update(app);
+    account(app);
     _status(app);
 };
