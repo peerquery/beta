dc97412110322b9eb82cf1c3fac9a0c56ee4ab66
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 23:02:26 +0000

    mount activity/notifications apis

--- src/apis/content/index.js
+++ src/apis/content/index.js
@@ -4,12 +4,16 @@ var reports = require('./reports'),
     queries = require('./queries'),
     projects = require('./projects'),
     peers = require('./peers'),
-    messages = require('./messages');
+    activity = require('./activity'),
+    messages = require('./messages'),
+    notifications = require('./notifications');
 
 module.exports = function(app) {
     reports(app);
     queries(app);
     projects(app);
     peers(app);
+    activity(app);
     messages(app);
+    notifications(app);
 };
