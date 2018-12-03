ac2ea55f4548121c1d9a726074a48b66c12861a5
dzivenu <makafuigdzivenu@gmail.com>
2018-12-03 23:30:52 +0000

    mount account routes

--- src/routes/indexes/secure.js
+++ src/routes/indexes/secure.js
@@ -2,10 +2,12 @@
 
 var client = require('../routes/client'),
     content = require('../routes/content'),
+    account = require('../routes/account'),
     office = require('../routes/office');
 
 module.exports = async function(app) {
     client(app);
     office(app);
+    account(app);
     content(app); //always mount this router last, else **/@:username/:permLink** will override some of the routes handled above
 };
