4ea23219a3fc71c02465647f1587b131bed798a2
dzivenu <makafuigdzivenu@gmail.com>
2018-12-05 12:21:18 +0000

    add messages/notifications to actions

--- src/apis/create/membership.js
+++ src/apis/create/membership.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var peer = require('../../models/peer'),
+    activity = require('../../models/activity'),
+    message = require('../../models/message'),
+    notification = require('../../models/notification'),
+    uuid = require('../../lib/helpers/uuid'),
     project = require('../../models/project');
 //do not worry about sanitizing req.body; already done in the server!
 
@@ -34,13 +38,8 @@ module.exports = function(app) {
                             'members.$.state': 'active',
                             'members.$.type': 'member',
                         },
-                    },
-                    { upsert: true }
-                );
-
-                let update_member_count = await project.updateOne(
-                    { slug_id: req.body.project_slug_id },
-                    { $inc: { member_count: 1 } }
+                        $inc: { member_count: 1 },
+                    }
                 );
 
                 let membership_updated = await peer.updateOne(
@@ -53,8 +52,8 @@ module.exports = function(app) {
                             'memberships.$.state': 'active',
                             'membership.$.type': 'member',
                         },
-                    },
-                    { upsert: true }
+                        $inc: { project_membership_count: 1 },
+                    }
                 );
 
                 if (approve_request && membership_updated) {
@@ -62,6 +61,36 @@ module.exports = function(app) {
                 } else {
                     res.sendStatus(500);
                 }
+
+                var newActivity = activity({
+                    slug_id: req.body.project_slug_id,
+                    action: 'approve_membership',
+                    account: req.active_user.account,
+                    created: Date.now(),
+                });
+
+                var project_messages = await project
+                    .findOne({ identifier: 'default' })
+                    .select('new_member_message_title new_member_message_body');
+
+                var newMessage = message({
+                    slug_id: uuid(),
+
+                    author: req.body.project_slug_id,
+                    recipient: req.body.account,
+
+                    title: project_messages.new_user_message_title,
+                    body: project_messages.new_user_message_body,
+
+                    event: 'project_membership_approved',
+                    state: 'pending',
+                    relation: 'project_2_user',
+                    created: Date.now(),
+                });
+
+                await newMessage.save();
+
+                await newActivity.save();
             }
         } catch (err) {
             console.log(err);
@@ -97,6 +126,7 @@ module.exports = function(app) {
                                 role: { $ne: 'owner' },
                             },
                         },
+                        $inc: { member_count: -1 },
                     }
                 );
 
@@ -109,15 +139,23 @@ module.exports = function(app) {
                                 role: { $ne: 'owner' },
                             },
                         },
+                        $inc: { project_membership_count: -1 },
                     }
                 );
 
-                let update_member_count = await project.updateOne(
-                    { slug_id: req.body.project_slug_id },
-                    { $inc: { member_count: -1 } }
-                );
-
                 if (leave_project && update_user) {
+                    var newNotification = notification({
+                        event: 'project_membership_removed',
+                        from: req.body.project_slug_id,
+                        from_account: req.active_user.account,
+                        relation: 'project_2_user',
+                        to: req.active_user.account,
+                        status: 'pending',
+                        created: Date.now(),
+                    });
+
+                    await newNotification.save();
+
                     res.sendStatus(200);
                 } else {
                     res.sendStatus(500);
@@ -150,6 +188,18 @@ module.exports = function(app) {
             );
 
             if (reject_request && update_user) {
+                var newNotification = notification({
+                    event: 'project_membership_rejected',
+                    from: req.body.project_slug_id,
+                    from_account: req.active_user.account,
+                    relation: 'project_2_user',
+                    to: req.active_user.account,
+                    status: 'pending',
+                    created: Date.now(),
+                });
+
+                await newNotification.save();
+
                 res.sendStatus(200);
             } else {
                 res.sendStatus(500);
@@ -218,8 +268,7 @@ module.exports = function(app) {
                     slug_id: req.body.project_slug_id,
                     'members.account': req.body.account,
                 },
-                { $set: { 'members.$.type': 'team' } },
-                { upsert: true }
+                { $set: { 'members.$.type': 'team' } }
             );
 
             let update_user = await peer.updateOne(
@@ -227,11 +276,22 @@ module.exports = function(app) {
                     account: req.body.account,
                     'memberships.slug_id': req.body.project_slug_id,
                 },
-                { $set: { 'memberships.$.type': 'team' } },
-                { upsert: true }
+                { $set: { 'memberships.$.type': 'team' } }
             );
 
             if (upgrade_membership && update_user) {
+                var newNotification = notification({
+                    event: 'project_membership_left',
+                    from: req.body.project_slug_id,
+                    from_account: req.active_user.account,
+                    relation: 'project_2_user',
+                    to: req.body.account,
+                    status: 'pending',
+                    created: Date.now(),
+                });
+
+                await newNotification.save();
+
                 res.sendStatus(200);
             } else {
                 res.sendStatus(500);
