f819b7b2eae13e5e30358cad069947146b17f6f5
dzivenu <makafuigdzivenu@gmail.com>
2018-12-05 13:40:44 +0000

    include default welcomers and informations

--- src/configs/config.js
+++ src/configs/config.js
@@ -16,8 +16,17 @@ config.steem_rpc = 'https://api.steemit.com';
 config.steem_account = 'peerquery';
 config.community = 'peerquery';
 
+//welcomers
+config.new_user_message_title = 'Hi there!';
+config.new_user_message_body =
+    'Welcome to the world of peer to peer collaborations on the Blockchain!. Visit our <a href="/support"> Support Center </a> to get started.';
+
+//informations
+config.new_project_message_title = 'Wow, that is great!';
+(config.new_project_message_body =
+    'You might want to create an introductory post to introduce your project to the community. Remember to share your project with your friend so as to get them to join!'),
 //sc2
-config.sc2_app_name = 'peerquery.app';
+(config.sc2_app_name = 'peerquery.app');
 config.sc2_scope = 'vote,comment,custom_json';
 config.sc2_scope_array = '[\'vote\', \'comment\', \'custom_json\']';
 
