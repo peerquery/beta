50903b00ecad0bb9ff1606c537a941114cc12686
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 13:29:14 +0000

    mount account

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,12 +3,14 @@
 var auth = require('./auth/index'),
     content = require('./content/index'),
     create = require('./create/index'),
+    account = require('./account/index'),
     manage = require('./manage/index'),
     _static = require('./static/index'),
     office = require('./office/index');
 
 module.exports = function(app) {
     auth(app);
+    account(app);
     content(app);
     create(app);
     manage(app);
