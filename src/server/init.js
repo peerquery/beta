0f195ad1352b15195331f1780e381251d27a2974
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 13:36:27 +0000

    call new scripts index

--- src/server/init.js
+++ src/server/init.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var db_setup = require('../configs/db');
+var db_setup = require('../scripts/db-init'),
+    scripts = require('../scripts/index');
 
 module.exports = async function() {
+    await scripts();
     if (await db_setup()) return true;
     return false;
 };
