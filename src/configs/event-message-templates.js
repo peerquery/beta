8ef30578dd90d65b45a03816db1fddc46820e69f
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 12:08:03 +0000

    rename and group active

--- src/configs/event-message-templates.js
+++ src/configs/event-message-templates.js
@@ -4,10 +4,6 @@
 
 var messages = {};
 
-messages.new_user_welcome =
-    'Thank you for joining {{to}}. Please read our <a href="/support/faqs">FAQs</a> or visit our <a href="/support">support center</a> to get started.';
-messages.new_user_reward_supporter =
-    'Wow, you just received a new post reward split support from @{{from}}.';
 messages.new_user_message = 'You have received a new message from @{{from}}.';
 messages.new_user_follow = '@{{from}} started following you!';
 messages.new_user_hire = '@{{from}} wants to hire you!';
@@ -16,20 +12,39 @@ messages.new_project_membership_request =
     '@{{from}} requested to join your project.';
 
 messages.new_project_member_welcome =
-    'Welcome to the {{from_name}} project. We hope to do great things together.'; //used project does not have a custom message for new members
+    'Welcome to the {{from}} project. We hope to do great things together.'; //used project does not have a custom message for new members
 
-messages.new_project_reward_supporter =
-    'Your project has received a new reward split support from @{{from}}';
 messages.new_project_follow = '@{{from}} started following your project.';
 
-messages.new_project_creation_message =
-    '{{from_name}} looks great. Remember to create queries to enlist like-minded peers to your project.';
-
 messages.project_membership_approved =
-    'Your membership request to {{from_name}} has been approved!';
+    'Your membership request to {{from}} has been approved!';
 messages.project_membership_declined =
-    'Your membership of request to {{from_name}} has been declined.';
+    'Your membership of request to {{from}} has been declined.';
 messages.project_membership_removed =
-    'Your membership of {{from_name}} has been removed.';
+    'Your membership of {{from}} has been removed.';
+
+/* active */
+
+//informators
+messages.project_membership_upgrade =
+    'You have been made a team member of the <a href="/project/{{from}}">{{from}}</a> project.';
+
+//thankers
+messages.sign_up =
+    'Thank you for joining <a href="/peer/{{to}}">{{from}}</a>. Please read our <a href="/support/faqs">FAQs</a> or visit our <a href="/support">support center</a> to get started.';
+
+messages.user_beneficiary =
+    'Wow, you just received a new post reward split support from <a href="/peer/{{from}}">@{{from}}</a>.';
+
+messages.project_beneficiary =
+    'Your project has received a new reward split support from <a href="/peer/{{from}}">@{{from}}</a>';
+
+//manage
+messages.create_project =
+    '<a href="/peer/{{from}}">@{{from}}</a> looks great. Remember to create queries to enlist like-minded peers to your project.';
+messages.tranfer_project =
+    'The <a href="/project/{{from}}">{{from}}</a> project has been transfered to you.';
+messages.delete_project =
+    'Your <a href="/project/{{from}}">{{from}}</a> project has been deleted. Remember, you can always create a new one.';
 
 module.exports = messages;
