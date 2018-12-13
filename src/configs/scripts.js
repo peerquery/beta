2a4630ec433853dbe7403d9d7674972b5e43dbde
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 14:44:38 +0000

    include scripts config

---
+++ src/configs/scripts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var settings = {};
+
+settings.migrate_membership = true;
+settings.force_steem_account = true;
+settings.force_membership_titles = true;
+
+module.exports = settings;
