791b1f8a874615e4798e439276b4c4f5796b516b
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 10:24:14 +0000

    rename fields, use auto timestamp for created/update

--- src/models/relation.js
+++ src/models/relation.js
@@ -3,19 +3,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var relationSchema = new Schema({
-    //details
-    followee: { type: String },
-    following: { type: String },
-    target: { type: String }, //project, user
-    state: { type: String }, //active, inactive(unscribed)
-    created: { type: Date, required: true, default: Date.now },
-    left: { type: Date, required: true, default: Date.now }, //unsubscripted data
-});
+var relationSchema = new Schema(
+    {
+        //details
+
+        identifier: { type: String, required: true, unique: true }, //in the format: followee_follower, eg: 'dzivenu_peerquery'
+
+        state: { type: String }, //active, inactive(subscribed)
+
+        following: { type: String, ref: 'peerSchema', required: true }, //the lucky person being followed
+        follower: { type: String, ref: 'peerSchema', required: true }, //the person doing the follow
+
+        benefactor_rate: { type: Number }, //if greater than 0, then is an active benefactor
+        benefactor_created: { type: Date },
+        benefactor_label: { type: String }, //only the peer has this
+        benefactor_message: { type: String },
+    },
+    {
+        timestamps: { createdAt: 'created', updatedAt: 'updated' },
+    }
+);
 
 relationSchema.index(
-    { followee: 1, subject: 1 },
-    { following: 1, subject: 1 },
+    { follower: 1, following: 1, updated: 1 },
     { name: 'relation_index' }
 );
 
