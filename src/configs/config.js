bc9f10b44f963d529008da2e723e0a449f417cc7
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 12:11:47 +0000

    include welcomers, thankers, include SC2 scope/comment_options

--- src/configs/config.js
+++ src/configs/config.js
@@ -21,14 +21,25 @@ config.new_user_message_title = 'Hi there!';
 config.new_user_message_body =
     'Welcome to the world of peer to peer collaborations on the Blockchain!. Visit our <a href="/support"> Support Center </a> to get started.';
 
+//default project welcomers
+config.new_member_message_title = 'You are our newest member!';
+config.new_member_message_body =
+    'We are happy to have you as part of our members. Explore our reports to see what we are actively working on.';
+
+//thankers
+config.new_benefactor_message_title = 'Many thanks for your support';
+config.new_benefactor_message_body =
+    'We will continue to work hard to bring our vision closer torwards realization. Reach us by sending us a message on our page.';
+
 //informations
 config.new_project_message_title = 'Wow, that is great!';
 (config.new_project_message_body =
     'You might want to create an introductory post to introduce your project to the community. Remember to share your project with your friend so as to get them to join!'),
 //sc2
 (config.sc2_app_name = 'peerquery.app');
-config.sc2_scope = 'vote,comment,custom_json';
-config.sc2_scope_array = '[\'vote\', \'comment\', \'custom_json\']';
+config.sc2_scope = 'vote,comment,comment_options,custom_json';
+config.sc2_scope_array =
+    '[\'vote\', \'comment\', \'comment_options\', \'custom_json\']';
 
 //attribution
 config.report_attribution =
@@ -49,5 +60,8 @@ config.curation_rest_day2 = 'sunday';
 config.curation_vote_interval_minutes = 20;
 config.curation_common_comment =
     'Nice work! Consider delegating Steem power to support Peer Query in its bit to support peer to peer collaborations.';
+
+//config.migrate_memberships = true;
+config.migrate_memberships = true;
 
 module.exports = config;
