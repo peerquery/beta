3ea90a380e56746da6e9dd2f695eb6c0cbb3de37
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 13:26:03 +0000

    include query update/delete, fetch new/popular for sidebar

--- src/apis/content/queries.js
+++ src/apis/content/queries.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var queries = require('../../models/query');
+var peer = require('../../models/peer');
+var project = require('../../models/project');
 
 module.exports = async function(app) {
     app.get('/api/fetch/queries/featured/:last_id', async function(req, res) {
@@ -121,6 +123,97 @@ module.exports = async function(app) {
             let query =
                 'image title description author permlink reward type reward_form project_title project_slug_id deadline created';
             let results = await queries.aggregate([{ $sample: { size: 20 } }]);
+            res.status(200).json(results);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+
+    app.get('/api/fetch/queries/popular/short', async function(req, res) {
+        try {
+            let results = await queries
+                .find({})
+                .select('author title permlink created view_count')
+                .sort({ view_count: -1 })
+                .limit(10);
+            res.status(200).json(results);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+
+    app.get('/api/fetch/queries/new/short', async function(req, res) {
+        try {
+            let results = await queries
+                .find({})
+                .select('author title permlink created view_count')
+                .sort({ _id: -1 })
+                .limit(10);
+            res.status(200).json(results);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+
+    app.post('/api/private/query/delete', async function(req, res) {
+        try {
+            let results = await queries.findOneAndDelete({
+                permlink: req.body.permlink,
+                author: req.active_user.account,
+            });
+            if (!results)
+                return res
+                    .status(405)
+                    .send('Query might not exist or you are not the author');
+
+            if (req.body.project_slug_id)
+                await project.updateOne(
+                    { slug_id: req.body.project_slug_id },
+                    { $inc: { query_count: -1 } }
+                );
+
+            await peer.updateOne(
+                { account: req.body.author },
+                { $inc: { query_count: -1 } }
+            );
+            res.status(200).json(results);
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+
+    app.post('/api/private/query/update', async function(req, res) {
+        try {
+            let update = {
+                title: req.body.title,
+                image: req.body.image,
+                description: req.body.description,
+                body: req.body.body,
+                reward_form: req.body.reward_form,
+                type: req.body.type,
+                label: req.body.label,
+                deadline: req.body.deadline,
+                telephone: req.body.telephone,
+                email: req.body.email,
+                website: req.body.website,
+            };
+
+            let results = await queries.findOneAndUpdate(
+                {
+                    permlink: req.body.permlink,
+                    author: req.active_user.account,
+                },
+                update
+            );
+            if (!results)
+                return res
+                    .status(405)
+                    .send('Query might not exist or you are not the author');
+
             res.status(200).json(results);
         } catch (err) {
             console.log(err);
