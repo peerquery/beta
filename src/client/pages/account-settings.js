46754f803d4aedfec7dcb364ef609818afb7f806
dzivenu <makafuigdzivenu@gmail.com>
2018-12-13 09:35:54 +0000

    support new benefactor thank you message

--- src/client/pages/account-settings.js
+++ src/client/pages/account-settings.js
@@ -19,6 +19,35 @@ $('#hiringBtn').click(function() {
     });
 });
 
+$('#benefactormessageBtn').click(function() {
+    //slightly throttle use of feature by disabling upon save, to re-use user must reload page
+    $(this).addClass('disabled');
+
+    var update = {
+        title: $('#benefactormessageTitle').val(),
+        body: $('#benefactormessageBody').val(),
+    };
+
+    if (!update.title) {
+        pqy_notify.warn(
+            'Benefactor"s thank you message title cannot be empty.'
+        );
+    } else if (!update.body) {
+        pqy_notify.warn('Benefactor"s thank you message body cannot be empty.');
+    } else {
+        $.post(
+            '/api/private/settings/update/benefactor_message',
+            update,
+            function(data, status) {
+                pqy_notify.success('Successfully updated account data.');
+            }
+        ).fail(function(err) {
+            pqy_notify.warn('An error occurred, pleased try again.');
+            //console.log(err.statusText);
+        });
+    }
+});
+
 $('#messagingBtn').click(function() {
     //slightly throttle use of feature by disabling upon save, to re-use user must reload page
     $(this).addClass('disabled');
